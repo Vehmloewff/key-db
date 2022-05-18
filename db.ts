0a53c567d20b2b1d59ae92a839219ea885f82992
Elijah Mooring <ejsisgreat@gmail.com>
2022-05-18 00:28:07 -0500

    feat: save on mongodb atlas

--- db.ts
+++ db.ts
@@ -1,10 +1,16 @@
-import { UserError } from './deps.ts'
+import { UserError, mongo } from './deps.ts'
+
+const DATABASE_URI = Deno.env.get('DATABASE_URI')
+if (!DATABASE_URI) throw new Error('Expected env var DATABASE_URI to be set')
+
+const client = new mongo.MongoClient()
+const database = await client.connect(DATABASE_URI)
 
 export interface GetFsPathOptions {
 	requireKey?: boolean
 }
 
-async function getFSPath(path: string, options: GetFsPathOptions = {}) {
+function getFSPath(path: string, options: GetFsPathOptions = {}) {
 	const [project, key] = path.split('/').filter(section => {
 		if (!section.length) return false
 		if (/^\.+$/.test(section)) return false
@@ -14,37 +20,35 @@ async function getFSPath(path: string, options: GetFsPathOptions = {}) {
 
 	if (!project) throw new UserError('Expected a project to be specified')
 
-	const root = 'data'
-	const base = `${root}/${project}`
+	if (!key && options.requireKey)
+		throw new UserError('This operation cannot be performed on a project as a whole.  A key must be specified.')
 
-	try {
-		await Deno.stat(base)
-	} catch (_) {
-		await Deno.mkdir(base, { recursive: true })
-	}
-
-	if (!key) {
-		if (options.requireKey) throw new UserError('This operation cannot be performed on a project as a whole.  A key must be specified.')
-
-		return `${base}.json`
-	}
-	return `${base}/${key}.json`
+	return { project, key }
 }
 
 export async function set(path: string, value: string) {
-	await Deno.writeTextFile(await getFSPath(path, { requireKey: true }), value)
+	const { key, project } = getFSPath(path, { requireKey: true })
+
+	const collection = database.collection(project)
+	await collection.insertOne({ data: value, key })
 }
 
 export async function get(path: string) {
-	const fsPath = await getFSPath(path, { requireKey: true })
+	const { project, key } = getFSPath(path, { requireKey: true })
 
-	try {
-		return await Deno.readTextFile(fsPath)
-	} catch (_) {
-		return null
-	}
+	const collection = database.collection(project)
+
+	const res = await collection.findOne({ key })
+	if (!res) return null
+
+	return res.data
 }
 
 export async function remove(path: string) {
-	await Deno.remove(await getFSPath(path), { recursive: true })
+	const { project, key } = getFSPath(path)
+
+	const collection = database.collection(project)
+
+	if (!key) await collection.drop()
+	return await collection.deleteOne({ key })
 }
--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export * from 'https://deno.land/std@0.139.0/http/mod.ts'
 export * from 'https://denopkg.com/Vehmloewff/deno-utils@1.1.0/lib/errors.ts'
+export * as mongo from 'https://deno.land/x/mongo@v0.29.4/mod.ts'
